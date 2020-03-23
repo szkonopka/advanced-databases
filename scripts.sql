fa850f0f04cf14633bbc3a8a3d54fc850fc4d7da
Grzegorz Stala <gstala01@gmail.com>
2020-03-23 09:08:38 +0100

    Scripts

--- scripts.sql
+++ scripts.sql
@@ -8,6 +8,31 @@ select G.GENERATIONNAME, AVG(S.LINEITEMAMOUNT) from CUSTOMER C
     join SALESRECIEPTS S on C.ID = S.CUSTOMER
 group by G.GENERATIONNAME;
 
+-- QUERY 2
+select * from (
+select s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME,so.STORECITY, SUM(round(pi.waste * sr.LINEITEMAMOUNT, 2)) as waste, 'GOOD' as status from STAFF s
+    join POSITION p on s.POSITION = p.ID
+    join SALESOUTLET so on s.ID = so.MANAGER
+    join SALESRECIEPTS sr on so.ID = sr.SALESOUTLET
+    join dates on sr.TRANSACTIONDATE = DATES.TRANSACTIONDATE
+    join PASTRYINVENTORY pi on DATES.TRANSACTIONDATE = pi.TRANSACTIONDATE
+    join PRODUCT on pi.PRODUCT = PRODUCT.ID AND sr.PRODUCT = PRODUCT.ID
+    WHERE months_between(sysdate, SR.TRANSACTIONDATE) < :months_threshold
+    group by s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME, so.STORECITY
+    order by WASTE) where ROWNUM <= :rownum
+union
+select * from (
+select s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME,so.STORECITY, SUM(round(pi.waste * sr.LINEITEMAMOUNT, 2)) as waste, 'BAD' as status from STAFF s
+    join POSITION p on s.POSITION = p.ID
+    join SALESOUTLET so on s.ID = so.MANAGER
+    join SALESRECIEPTS sr on so.ID = sr.SALESOUTLET
+    join dates on sr.TRANSACTIONDATE = DATES.TRANSACTIONDATE
+    join PASTRYINVENTORY pi on DATES.TRANSACTIONDATE = pi.TRANSACTIONDATE
+    join PRODUCT on pi.PRODUCT = PRODUCT.ID AND sr.PRODUCT = PRODUCT.ID
+    WHERE months_between(sysdate, SR.TRANSACTIONDATE) < :months_threshold
+    group by s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME, so.STORECITY
+    order by WASTE DESC) where ROWNUM <= :rownum;
+
 -- QUERY 3
 select * from (
 select P.ID, P.PRODUCTCATEGORY, PY.WASTEPERCENTAGE, D.TRANSACTIONDATE, 'GOOD' as status from GRZEGORZ.PRODUCT P
@@ -35,6 +60,14 @@ select S.ID, S.FIRSTNAME, s.LASTNAME, p.NAME, COUNT(*) as sales, 'GOOD' as statu
     join SALESRECIEPTS sr on s.ID = sr.STAFF
     join dates d on sr.TRANSACTIONDATE = d.TRANSACTIONDATE
     group by s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME order by COUNT(*) DESC) res2 where ROWNUM <= 10;
+
+-- QUERY 5
+select cus.id, cus.FIRSTNAME, g.GENERATIONNAME, so.ID as salesOutletId, COUNT(*) as sales, SUM(sr.LINEITEMAMOUNT) as total from CUSTOMER cus
+    join SALESRECIEPTS sr on cus.ID = sr.CUSTOMER
+    join SALESOUTLET so on sr.SALESOUTLET = so.ID
+    join GENERATION G on cus.BIRTHYEAR = G.BIRTHYEAR
+    where sr.TRANSACTIONDATE < to_date('05-04-19','DD-MM-YY') and ROWNUM <= :limit
+    group by cus.ID, cus.FIRSTNAME, g.GENERATIONNAME, so.ID order by SUM(sr.LINEITEMAMOUNT) DESC ;
 
 -- ZESTAW JAKUB STEPANIAK
 -- QUERY 1
