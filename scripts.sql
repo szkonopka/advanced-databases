b887aeebfda4d772b083d19a3fa88bc3a4f8e255
Grzegorz Stala <gstala01@gmail.com>
2020-03-23 09:39:48 +0100

    Scripts

--- scripts.sql
+++ scripts.sql
@@ -3,7 +3,7 @@
 -- ZESTAW GRZEGORZ STALA
 
 -- QUERY 1
-select G.GENERATIONNAME, AVG(S.LINEITEMAMOUNT) from CUSTOMER C
+select G.GENERATIONNAME, ROUND(AVG(S.LINEITEMAMOUNT),2) as avgLineItemAmount from CUSTOMER C
     join GENERATION G on C.BIRTHYEAR = G.BIRTHYEAR
     join SALESRECIEPTS S on C.ID = S.CUSTOMER
 group by G.GENERATIONNAME;
@@ -38,13 +38,13 @@ select * from (
 select P.ID, P.PRODUCTCATEGORY, PY.WASTEPERCENTAGE, D.TRANSACTIONDATE, 'GOOD' as status from GRZEGORZ.PRODUCT P
     join GRZEGORZ.PASTRYINVENTORY PY on P.ID = PY.PRODUCT
     join DATES D on PY.TRANSACTIONDATE = D.TRANSACTIONDATE
-    order BY PY.WASTEPERCENTAGE) res1 where res1.TRANSACTIONDATE < to_date('05-04-19','DD-MM-YY') AND ROWNUM <= 10
+    order BY PY.WASTEPERCENTAGE) res1 where months_between(sysdate, res1.TRANSACTIONDATE) < :months_threshold AND ROWNUM <= :limit
 union
 select * from (
 select P.ID, P.PRODUCTCATEGORY, PY.WASTEPERCENTAGE, D.TRANSACTIONDATE, 'BAD' as status from GRZEGORZ.PRODUCT P
     join GRZEGORZ.PASTRYINVENTORY PY on P.ID = PY.PRODUCT
     join DATES D on PY.TRANSACTIONDATE = D.TRANSACTIONDATE
-    order BY PY.WASTEPERCENTAGE DESC) res2  where res2.TRANSACTIONDATE < to_date('05-04-19','DD-MM-YY') AND ROWNUM <= 10;
+    order BY PY.WASTEPERCENTAGE DESC) res2  where months_between(sysdate, res2.TRANSACTIONDATE) < :months_threshold AND ROWNUM <= :limit;
 
 -- QUERY 4
 select * from (
@@ -52,22 +52,34 @@ select S.ID, S.FIRSTNAME, s.LASTNAME, p.NAME, COUNT(*) as sales, 'BAD' as status
     join POSITION p on s.POSITION = p.ID
     join SALESRECIEPTS sr on s.ID = sr.STAFF
     join dates d on sr.TRANSACTIONDATE = d.TRANSACTIONDATE
-    group by s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME order by COUNT(*)) res1 where ROWNUM <= 10
+    group by s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME order by COUNT(*)) res1 where ROWNUM <= :limit
 union
 select * from (
 select S.ID, S.FIRSTNAME, s.LASTNAME, p.NAME, COUNT(*) as sales, 'GOOD' as status from STAFF s
     join POSITION p on s.POSITION = p.ID
     join SALESRECIEPTS sr on s.ID = sr.STAFF
     join dates d on sr.TRANSACTIONDATE = d.TRANSACTIONDATE
-    group by s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME order by COUNT(*) DESC) res2 where ROWNUM <= 10;
+    group by s.ID, s.FIRSTNAME, s.LASTNAME, p.NAME order by COUNT(*) DESC) res2 where ROWNUM <= :limit;
 
 -- QUERY 5
 select cus.id, cus.FIRSTNAME, g.GENERATIONNAME, so.ID as salesOutletId, COUNT(*) as sales, SUM(sr.LINEITEMAMOUNT) as total from CUSTOMER cus
     join SALESRECIEPTS sr on cus.ID = sr.CUSTOMER
     join SALESOUTLET so on sr.SALESOUTLET = so.ID
     join GENERATION G on cus.BIRTHYEAR = G.BIRTHYEAR
-    where sr.TRANSACTIONDATE < to_date('05-04-19','DD-MM-YY') and ROWNUM <= :limit
+    where months_between(sysdate, sr.TRANSACTIONDATE) < :months_threshold and ROWNUM <= :limit
     group by cus.ID, cus.FIRSTNAME, g.GENERATIONNAME, so.ID order by SUM(sr.LINEITEMAMOUNT) DESC ;
+
+-- COMMAND 1
+update STAFF
+    set POSITION = (select id from POSITION where NAME = :name)
+    where ID in (
+        select * from (
+select st.ID from staff st
+    join SALESRECIEPTS sr on st.ID = sr.STAFF
+    WHERE months_between(sysdate, SR.TRANSACTIONDATE) < :months_threshold
+    group by st.ID, st.FIRSTNAME, st.LASTNAME
+    order by count(*) DESC) where ROWNUM <= :limit
+              );
 
 -- ZESTAW JAKUB STEPANIAK
 -- QUERY 1
