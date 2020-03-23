ea33ca69f8a291e9bbdf5084763e1abeab57a39e
Kuba Stepaniak <kuba.stepaniak@gmail.com>
2020-03-23 14:37:36 +0100

    Queries and commands from set 1 updated.

--- scripts.sql
+++ scripts.sql
@@ -34,6 +34,152 @@ select st.YEARMONTHDATE, so.ID as salesOutletId, st.TOTALGOAL, COUNT(*) as sales
     where so.ID = :salesOutletId
     group by st.YEARMONTHDATE, st.TOTALGOAL, so.ID;
 
+-- QUERY 5
+select GENERATIONNAME,
+       pp.DESCRIPTION,
+       round(100 * count(*) / sum(count(*)) over (partition by pp.DESCRIPTION), 2) percentage_by_product
+from customer cc
+         join GENERATION gg on cc.BIRTHYEAR = gg.BIRTHYEAR
+         join SALESRECIEPTS sr on cc.ID = sr.CUSTOMER
+         join product pp on sr.PRODUCT = pp.ID
+where sr.SALESOUTLET = 3
+group by GENERATIONNAME, pp.DESCRIPTION
+order by pp.DESCRIPTION;
+
+-- COMMAND 1
+create or replace procedure Sell_Product(in_product_id number,
+                                         in_sales_outlet_id number,
+                                         in_staff_id number,
+                                         in_customer_id number,
+                                         in_transaction_id number,
+                                         in_quantity number,
+                                         in_line_item_amount number,
+                                         in_unit_price number,
+                                         in_promo number)
+    is
+begin
+    insert into SALESRECIEPTS (TRANSACTIONTIME,
+                               TRANSACTIONDATE,
+                               TRANSACTIONID,
+                               QUANTITY,
+                               LINEITEMAMOUNT,
+                               UNITPRICE,
+                               PROMO,
+                               SALESOUTLET,
+                               PRODUCT,
+                               STAFF,
+                               CUSTOMER,
+                               INSTORE,
+                               "order",
+                               LINEITEMID)
+    values (trunc(sysdate),
+            sysdate,
+            in_transaction_id,
+            in_quantity,
+            in_line_item_amount,
+            in_unit_price,
+            in_promo,
+            in_sales_outlet_id,
+            in_product_id,
+            in_staff_id,
+            in_customer_id,
+            1,
+            1,
+            1);
+end;
+
+begin
+    SELL_PRODUCT(
+        1,
+        2,
+        1,
+        1,
+        2,
+        3,
+        50,
+        1,
+        in_promo => 1);
+end;
+
+-- COMMAND 2
+update SALESTARGET st
+set st.TOTALGOAL       = :change * nvl((
+                                           select sum(QUANTITY * UNITPRICE)
+                                           from SALESRECIEPTS sr
+                                                    join SALESOUTLET so on sr.SALESOUTLET = so.ID
+                                                    join SALESTARGET st on so.ID = st.SALESOUTLET
+                                                    join PRODUCT pp on sr.PRODUCT = pp.ID
+                                                    join PRODUCTGROUP pg on pp.PRODUCTGROUP = pg.ID
+                                           where so.ID = :outlet_id
+                                       ), st.TOTALGOAL),
+    st.FOODGOAL        = :change * nvl((
+                                           select sum(QUANTITY * UNITPRICE)
+                                           from SALESRECIEPTS sr
+                                                    join SALESOUTLET so on sr.SALESOUTLET = so.ID
+                                                    join SALESTARGET st on so.ID = st.SALESOUTLET
+                                                    join PRODUCT pp on sr.PRODUCT = pp.ID
+                                                    join PRODUCTGROUP pg on pp.PRODUCTGROUP = pg.ID
+                                           where so.ID = :outlet_id
+                                             and "group" = 'Food'
+                                           group by "group"
+                                       ), st.FOODGOAL),
+    st.BEANSGOAL       = :change * nvl((
+                                           select sum(QUANTITY * UNITPRICE)
+                                           from SALESRECIEPTS sr
+                                                    join SALESOUTLET so on sr.SALESOUTLET = so.ID
+                                                    join SALESTARGET st on so.ID = st.SALESOUTLET
+                                                    join PRODUCT pp on sr.PRODUCT = pp.ID
+                                                    join PRODUCTGROUP pg on pp.PRODUCTGROUP = pg.ID
+                                           where so.ID = :outlet_id
+                                             and "group" = 'Whole Bean/Teas'
+                                           group by "group"
+                                       ), st.BEANSGOAL),
+    st.BEVERAGEGOAL    = :change * nvl((
+                                           select sum(QUANTITY * UNITPRICE)
+                                           from SALESRECIEPTS sr
+                                                    join SALESOUTLET so on sr.SALESOUTLET = so.ID
+                                                    join SALESTARGET st on so.ID = st.SALESOUTLET
+                                                    join PRODUCT pp on sr.PRODUCT = pp.ID
+                                                    join PRODUCTGROUP pg on pp.PRODUCTGROUP = pg.ID
+                                           where so.ID = :outlet_id
+                                             and "group" = 'Beverages'
+                                           group by "group"
+                                       ), st.BEVERAGEGOAL),
+    st.MERCHANDISEGOAL = :change * nvl((
+                                           select sum(QUANTITY * UNITPRICE)
+                                           from SALESRECIEPTS sr
+                                                    join SALESOUTLET so on sr.SALESOUTLET = so.ID
+                                                    join SALESTARGET st on so.ID = st.SALESOUTLET
+                                                    join PRODUCT pp on sr.PRODUCT = pp.ID
+                                                    join PRODUCTGROUP pg on pp.PRODUCTGROUP = pg.ID
+                                           where so.ID = :outlet_id
+                                             and "group" = 'Merchandise'
+                                           group by "group"
+                                       ), st.MERCHANDISEGOAL)
+where st.SALESOUTLET = :outlet_id;
+
+-- COMMAND 3
+update PRODUCT pp
+set pp.CURRENTWHOLESALEPRICE = (100 - :perc_discount) / 100 * pp.CURRENTWHOLESALEPRICE,
+    pp.CURRENTRETAILPRICE    = (100 - :perc_discount) / 100 * pp.CURRENTRETAILPRICE
+where pp.id in (
+    select ppp.id
+    from PRODUCT ppp
+             join SALESRECIEPTS sr on ppp.ID = sr.PRODUCT
+             join DATES dd on sr.TRANSACTIONDATE = dd.TRANSACTIONDATE
+    where dd.QUARTERNAME = :quarter
+      and dd.YEARID = :year
+    group by ppp.id
+    having sum(sr.UNITPRICE * sr.QUANTITY) < (
+        select sum(srr.UNITPRICE * srr.QUANTITY)
+        from SALESRECIEPTS srr
+                 join PRODUCT pppp on srr.PRODUCT = pppp.ID and pppp.ID = ppp.id
+                 join dates ddd on srr.TRANSACTIONDATE = ddd.TRANSACTIONDATE
+        group by pppp.ID
+        having max(ddd.TRANSACTIONDATE) < add_months(max(dd.TRANSACTIONDATE), -12)
+    )
+);
+
 -- ZESTAW GRZEGORZ STALA
 
 -- QUERY 1
