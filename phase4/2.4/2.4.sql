5f5bfb4be5574631c25ae1d7bcab7c0e90c571a6
Kuba Stepaniak <kuba.stepaniak@gmail.com>
2020-05-24 17:06:44 +0200

    2.4, 2.7 added.

--- phase4/2.4/2.4.sql
+++ phase4/2.4/2.4.sql
@@ -2,9 +2,18 @@ alter system flush buffer_cache;
 alter system flush shared_pool;
 set autotrace traceonly statistics
 
-TIMING START 2.4;
+TIMING START 2.4_1;
 
-SELECT 
+SELECT AVG(SDO_GEOM.SDO_DISTANCE(c.localization, so.localization, 0.005, 'unit=KM')) as distance
+FROM customer c JOIN sales_outlet so ON c.home_sales_outlet_id = so.id;
+
+TIMING STOP;
+
+TIMING START 2.4_1;
+
+SELECT AVG(SQRT(POWER(c.localization.sdo_point.x - so.localization.sdo_point.x, 2) + 
+POWER(COS(so.localization.sdo_point.x * 3.14/180) * c.localization.sdo_point.y - so.localization.sdo_point.y, 2)) * 40075.704/360) distance 
+FROM customer c JOIN sales_outlet so ON c.home_sales_outlet_id = so.id;
 
 TIMING STOP;
 
--- phase4/2.7/2.7.sql
+++ phase4/2.7/2.7.sql
@@ -4,7 +4,28 @@ set autotrace traceonly statistics
 
 TIMING START 2.7;
 
-SELECT 
+DECLARE
+    number_of_clients NUMBER(10,0) := 10;
+    new_range NUMBER(10,2) := 0;
+    outlet_id NUMBER(10,0) := 1;
+BEGIN     
+    SELECT distance
+    INTO new_range
+    FROM (
+        SELECT distance FROM (
+            SELECT c.id, SDO_GEOM.SDO_DISTANCE(c.localization, so.localization, 0.005, 'unit=KM') as distance
+            FROM customer c JOIN sales_outlet so ON c.home_sales_outlet_id = so.id
+            ORDER BY distance asc
+        )
+        WHERE ROWNUM <= number_of_clients
+        ORDER BY distance desc
+    )    
+    WHERE ROWNUM <= 1;
+    
+    UPDATE sales_outlet
+    SET delivery_range = SDO_UTIL.CIRCLE_POLYGON(longitude,latitude, new_range, 5)
+    WHERE id = outlet_id;
+END;
 
 TIMING STOP;
 
